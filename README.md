9aa8c2b10e91d0d9e347d8f307512bd6800a536d
devsara <devsara@qq.com>
2018-08-01 10:09:41 +0800

    下拉刷新数据

--- README.md
+++ README.md
@@ -8,4 +8,11 @@
     2)映射中英文天气
 
 # -动态显示背景图片和导航条
-    
+
+
+# -下拉刷新
+    -开启下拉刷新
+      1） 在window中添加属性，并设置为true，改变
+      2） 将"点点点"显示出来，配置 backgroundTextStyle 为 dark
+    -结束下拉刷新
+      1） 当获取完当前数据后，执行结束刷新x.stopPullDownRefresh()
--- pages/index/index.js
+++ pages/index/index.js
@@ -27,12 +27,28 @@ Page({
     nowWeatherBgc: ""
   },
 
+  // 添加下拉刷新处理函数---执行获取当前天气
+  onPullDownRefresh() {
+    // 获取完数据后---结束下拉刷新
+    this.getNowWeather( () => {
+      wx.stopPullDownRefresh();
+    });
+  },
+
+  // 页面加载的时候---执行获取当前天气
   onLoad() {
+    this.getNowWeather();
+  },  
+
+
+  // 封装函数，获取当前天气
+  getNowWeather(callback) {
+    
     // 发起网路请求，获取天气数据
     wx.request({
-      url: 'https://test-miniprogram.com/api/weather/now', 
+      url: 'https://test-miniprogram.com/api/weather/now',
       data: {
-        city:'北京'
+        city: '北京'
       },
       header: {
         'content-type': 'application/json' // 默认值
@@ -41,19 +57,23 @@ Page({
         let { data: resdata } = res;
         console.log(resdata);
         // 设置当前温度和天气
-        let { result: {now} } = resdata;
+        let { result: { now } } = resdata;
         console.log(now.temp, now.weather);
         // 必须使用this.setData 更行data中的数据，并将中英文映射
         this.setData({
           nowTemp: now.temp,
           nowWeather: weatherMap[now.weather],
-          nowWeatherBgc:'/img/' + now.weather + '-bg.png'
+          nowWeatherBgc: '/img/' + now.weather + '-bg.png'
         });
         // 动态改变导航条的颜色
         wx.setNavigationBarColor({
           frontColor: '#000000',
           backgroundColor: weatherColorMap[now.weather],
         })
+      },
+      // 获取完数据后执行的回调函数,如果有的话
+      complete: () => {
+        callback && callback()
       }
     })
   }
