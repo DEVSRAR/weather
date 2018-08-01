5378796c9a7e2f2d9953e6c942727c2ac96728b4
devsara <devsara@qq.com>
2018-08-01 12:50:04 +0800

    24h代码重构

--- pages/index/index.js
+++ pages/index/index.js
@@ -41,7 +41,7 @@ Page({
     this.getNowWeather();
   },  
 
-  // 封装函数，获取当前天气
+  // 封装函数---获取当前天气
   getNowWeather(callback) {
 
     // 发起网路请求，获取天气数据
@@ -54,53 +54,55 @@ Page({
         'content-type': 'application/json' // 默认值
       },
       success: res => {
-
         let { data: resdata } = res;
         console.log(resdata);
-
-        // 设置当前温度和天气
-        let { result: { now } } = resdata;
-        console.log(now.temp, now.weather);
-
-        // 获取未来24h的天气的数据
-        let forecastData = resdata.result.forecast;
-        console.log(forecastData);
-
-        // 必须使用this.setData 更行data中的数据，并将中英文映射
-        this.setData({
-          nowTemp: now.temp,
-          nowWeather: weatherMap[now.weather],
-          nowWeatherBgc: '/img/' + now.weather + '-bg.png'
-        });
-        // 动态改变导航条的颜色
-        wx.setNavigationBarColor({
-          frontColor: '#000000',
-          backgroundColor: weatherColorMap[now.weather],
-        });
-
-        // 定义一个空数组，构造成需要的格式
-        let future =[];
-        // 获取当前时间
-        let nowHover = new Date().getHours();
-
-        for( let i = 0; i < 24; i += 3) {
-          future.push({
-            time: (i + nowHover) % 24 + '时',
-            pic: "/img/" + forecastData[i/3].weather + "-icon.png",
-            temp: forecastData[i / 3].temp + '°'
-          })
-        }
-        // 让当前时间为 现在 
-        future[0].time = '现在';
-        this.setData({
-          future: future
-        })
-
+        this.setNow(resdata);
+        this.setHoverWeather(resdata);
       },
       // 获取完数据后执行的回调函数,如果有的话
       complete: () => {
         callback && callback()
       }
+    })
+  },
+  // 封装函数---设置当前的温度和天气
+  setNow(resdata) {
+    // 设置当前温度和天气
+    let { result: { now } } = resdata;
+    console.log(now.temp, now.weather);
+    // 必须使用this.setData 更行data中的数据，并将中英文映射
+    this.setData({
+      nowTemp: now.temp,
+      nowWeather: weatherMap[now.weather],
+      nowWeatherBgc: '/img/' + now.weather + '-bg.png'
+    });
+    // 动态改变导航条的颜色
+    wx.setNavigationBarColor({
+      frontColor: '#000000',
+      backgroundColor: weatherColorMap[now.weather],
+    });
+  },
+
+  // 封装函数---设置24h天气的变化
+  setHoverWeather(resdata) {
+    // 获取未来24h的天气的数据
+    let forecastData = resdata.result.forecast;
+    console.log(forecastData);
+    // 定义一个空数组，构造成需要的格式
+    let future = [];
+    // 获取当前时间
+    let nowHover = new Date().getHours();
+    for (let i = 0; i < 24; i += 3) {
+      future.push({
+        time: (i + nowHover) % 24 + '时',
+        pic: "/img/" + forecastData[i / 3].weather + "-icon.png",
+        temp: forecastData[i / 3].temp + '°'
+      })
+    }
+    // 让当前时间为 现在 
+    future[0].time = '现在';
+    this.setData({
+      future: future
     })
   }
 })
